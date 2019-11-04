51d750171886b0f53ea5f800e6066778c058bb8e
nathan <tedmax100@gmail.com>
2019-11-05 01:08:51 +0800

    add sample

--- service/main.go
+++ service/main.go
@@ -1,20 +1,22 @@
-package main 
+package main
 
 import (
 	"context"
 
-	"sparrow/sparrow/db"
-	"math/rand"
-	"time"
 	"fmt"
+	"math/rand"
+	"sparrow/sparrow/db"
+	"time"
 	//  "sync"
 )
+
 const charset = "abcdefghijklmnopqrstuvwxyz1234567890"
+
 var seededRand *rand.Rand = rand.New(
 	rand.NewSource(time.Now().UnixNano()))
 
 func StringWithCharset() string {
-	length := rand.Intn((15-6) + 6)
+	length := rand.Intn((15 - 6) + 6)
 
 	b := make([]byte, length)
 
@@ -25,17 +27,17 @@ func StringWithCharset() string {
 }
 
 type User struct {
-	ID int64
-	Name string
+	ID      int64
+	Name    string
 	Balance int
 }
 
 type Tran struct {
-	ID int64 `sql:"id"`
-	UserID int64`sql:"user_id"`
-	Amount int 
-	Type uint
-	Time  int64
+	ID     int64 `sql:"id"`
+	UserID int64 `sql:"user_id"`
+	Amount int
+	Type   uint
+	Time   int64
 }
 
 func main() {
@@ -45,73 +47,72 @@ func main() {
 	dbCtx := dbClient.Open(cancelCtx)
 
 	user := User{
-		ID : time.Now().UnixNano() / 1000 / 1000 / 1000,
-		Name : StringWithCharset(),
-		Balance : 1000,
+		ID:      time.Now().UnixNano() / 1000 / 1000 / 1000,
+		Name:    StringWithCharset(),
+		Balance: 1000,
 	}
 	insertCom := dbCtx.NewCommand(cancelCtx)
 	_, err := insertCom.For("user").Insert("id", user.ID).Insert("name", user.Name).Insert("balance", user.Balance).Exec()
+
 	if err != nil {
 		fmt.Println(err)
 	}
 
-/* 	var wg sync.WaitGroup 
-	for k:= 1 ; k < 100 ; k++ {
-		wg.Add(5)
-		for i := 0 ; i< 5; i ++ {
-			 go func() {
-				defer wg.Done()
-				tranId, err := dbClient.NewID(cancelCtx, "tran", "deposit")
-				if err != nil {
-					fmt.Println(err)
-				}
-				insertCom := dbCtx.NewCommand(cancelCtx)
-			
-				tran := Tran{
-					ID : tranId,
-					UserID: user.ID,
-					Amount: int(rand.Intn((100-6) + 6)),
-					Type : 1,
-					Time : time.Now().UnixNano() / 1000 / 1000/ 1000,
-				}
-		
-				_, err = insertCom.For("tran").Insert("id", tran.ID).Insert("user_id", tran.UserID).Insert("amount", tran.Amount).Insert("type", tran.Type).Insert("time", tran.Time).Exec()
-				if err != nil {
-					fmt.Println(err)
-				}
-			 }()
-			}
-		wg.Wait()
-		 time.Sleep(10 * time.Millisecond)
-	} */
-	
+	/* 	var wg sync.WaitGroup
+	   	for k:= 1 ; k < 100 ; k++ {
+	   		wg.Add(5)
+	   		for i := 0 ; i< 5; i ++ {
+	   			 go func() {
+	   				defer wg.Done()
+	   				tranId, err := dbClient.NewID(cancelCtx, "tran", "deposit")
+	   				if err != nil {
+	   					fmt.Println(err)
+	   				}
+	   				insertCom := dbCtx.NewCommand(cancelCtx)
+
+	   				tran := Tran{
+	   					ID : tranId,
+	   					UserID: user.ID,
+	   					Amount: int(rand.Intn((100-6) + 6)),
+	   					Type : 1,
+	   					Time : time.Now().UnixNano() / 1000 / 1000/ 1000,
+	   				}
+
+	   				_, err = insertCom.For("tran").Insert("id", tran.ID).Insert("user_id", tran.UserID).Insert("amount", tran.Amount).Insert("type", tran.Type).Insert("time", tran.Time).Exec()
+	   				if err != nil {
+	   					fmt.Println(err)
+	   				}
+	   			 }()
+	   			}
+	   		wg.Wait()
+	   		 time.Sleep(10 * time.Millisecond)
+	   	} */
+
 	var tranID int64
 	tranID, err = dbClient.NewID(cancelCtx, "tran", "deposit")
 	if err != nil {
 		fmt.Println(err)
 	}
 	insertCom = dbCtx.NewCommand(cancelCtx)
-			
-
 
 	tran := Tran{
-		ID : tranID,
+		ID:     tranID,
 		UserID: user.ID,
-		Amount: int(rand.Intn((100-6) + 6)),
-		Type : 1,
-		Time : time.Now().UnixNano() / 1000 / 1000/ 1000,
+		Amount: int(rand.Intn((100 - 6) + 6)),
+		Type:   1,
+		Time:   time.Now().UnixNano() / 1000 / 1000 / 1000,
 	}
 
 	insertCom.For("tran").Insert("id", tran.ID).Insert("user_id", tran.UserID).Insert("amount", tran.Amount).Insert("type", tran.Type).Insert("time", tran.Time)
 	err = dbCtx.Begin(insertCom)
 	if err != nil {
 		fmt.Println(err)
-	} 
+	}
 	_, err = insertCom.Exec()
 	if err != nil {
 		fmt.Println(err)
 		dbCtx.Rollback()
-	} 
+	}
 
 	queryUser := User{}
 	queryCom := dbCtx.NewQuery(cancelCtx)
@@ -122,5 +123,47 @@ func main() {
 	if err != nil {
 		dbCtx.Rollback()
 	}
-	dbCtx.Commit()
+	if queryUser.Balance-550 >= 0 {
+		tranID, err = dbClient.NewID(cancelCtx, "tran", "deposit")
+		if err != nil {
+			fmt.Println(err)
+		}
+		tran = Tran{
+			ID:     tranID,
+			UserID: user.ID,
+			Amount: 550,
+			Type:   1,
+			Time:   time.Now().UnixNano() / 1000 / 1000 / 1000,
+		}
+		insertCom.For("tran").Insert("id", tran.ID).Insert("user_id", tran.UserID).Insert("amount", tran.Amount).Insert("type", tran.Type).Insert("time", tran.Time)
+		_, err = insertCom.Exec()
+		if err != nil {
+			fmt.Println(err)
+			dbCtx.Rollback()
+		} else {
+			updateCom := dbCtx.NewCommand(cancelCtx)
+			updateCom.For("user").Update("balance", queryUser.Balance-550).Where("id={id}", "id")
+			vars := map[string]interface{}{"id": 1234512}
+			updateCom.Vars(vars)
+			updateResult, err := updateCom.Exec()
+			if err != nil {
+				fmt.Println(err)
+				dbCtx.Rollback()
+			} else {
+				rowAffected, err := updateResult.RowsAffected()
+				if err != nil {
+					fmt.Println(err)
+					dbCtx.Rollback()
+				}
+				if rowAffected == 0 {
+					dbCtx.Rollback()
+				} else {
+					dbCtx.Commit()
+				}
+
+			}
+		}
+
+	}
+
 }
