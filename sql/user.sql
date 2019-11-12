7a5181bb7de958546ae9b6cae9ac3d91b4355105
nathan_lu <nathan_lu@paradise-soft.com.tw>
2019-11-12 09:42:19 +0800

    not null

--- sql/user.sql
+++ sql/user.sql
@@ -3,6 +3,6 @@ USE lottery;
 CREATE TABLE `user` (
   `id` bigint(20) unsigned NOT NULL,
   `name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `balance` int(10) unsigned zerofill DEFAULT NULL,
+  `balance` int(10) unsigned zerofill NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
